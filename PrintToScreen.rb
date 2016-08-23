4afcfc43062a215d071f23907d8cd240ab3a2016
Cyndi Bolton <cbolton@wvtpg.com>
2016-08-23 13:17:14 -0400

    Replace 5

--- PrintToScreen.rb
+++ PrintToScreen.rb
@@ -1,13 +1,20 @@
- #this will output numbers 1-100 and replace 3 with Mined
+ #this will output numbers 1-100 
+ 
  x = 1
 100. times do
+#when a number is exactly divisible by 3, replace with Mined
 	if x % 3 == 0
 		puts "Mined"
-		
+
+#replace 5 with Minds	
+	elsif x == 5
+		puts "Minds"
+
+#when a number is not exactly divisible by 3, display number	
 	else
 	puts x	
 	end
-	puts x
+	
 	x = x + 1
 end
 
