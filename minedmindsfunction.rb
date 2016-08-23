f3d18f9c3c94147338eda4c3b8fbe78489ffc3bb
Cyndi Bolton <cbolton@wvtpg.com>
2016-08-23 14:47:29 -0400

    move function to code file

--- minedmindsfunction.rb
+++ minedmindsfunction.rb
@@ -1,27 +1,3 @@
- #this will output numbers 1-100 
- 
- x = 1
-100. times do
-	if x % 15 == 0
-		puts "Mined Minds"
-
-#when a number is exactly divisible by 3 replace with Mined
-	
-	elsif x % 3 == 0
-		puts "Mined"
-#when a number is exactly divisible by 5 replace with Mined
-
-	elsif x % 5 == 0
-		puts "Minds"
-
-#when a number is not exactly divisible by 3 and 5, display number	
-	else
-	puts x	
-	end
-	
-	x = x + 1
-end
-
-
-
-
+ def mined_minds(number)
+ 	1
+ end
--- testminedmindsfunction.rb
+++ testminedmindsfunction.rb
@@ -1,8 +1,14 @@
  require "minitest/autorun"
+require_relative "minedmindsfunction.rb"
 
  class TestMinedMindsFunction <Minitest::Test 
  	def test_1_equals_1
  		assert_equal(1, 1)
  	end
- 	
+
+
+ 	def test_1_returns_1
+ 		assert_equal(1, mined_minds(1))
+ 	end
+
  end
