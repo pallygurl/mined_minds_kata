c991a7ed260d56540e81c8d90288f9cfb11b39e6
Cyndi Bolton <cbolton@wvtpg.com>
2016-08-23 11:02:23 -0400

    First commit

---
+++ PrintToScreen.rb
@@ -0,0 +1,12 @@
+ #this will output numbers 1-100
+x = 1
+100. times do
+	if number ==3
+		puts "Mined"
+	puts x
+		x = x+1
+end
+
+
+
+
---
+++ replace3.rb
@@ -0,0 +1,10 @@
+#this will output numbers 1-100
+number = 1
+100. times do
+	if number ==3
+		puts "Mined"
+	else
+		puts number
+	end
+	number = number + 1
+end
