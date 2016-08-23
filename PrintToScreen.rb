ae43aebc11794c91b9a6c57dbf720150cc4a3577
Cyndi Bolton <cbolton@wvtpg.com>
2016-08-23 11:46:40 -0400

    Replace 3 with Mined

--- PrintToScreen.rb
+++ PrintToScreen.rb
@@ -1,6 +1,12 @@
- #this will output numbers 1-100
+ #this will output numbers 1-100 and replace 3 with Mined
  x = 1
 100. times do
+	if x == 3
+		puts "Mined"
+		
+	else
+	puts x	
+	end
 	puts x
 	x = x + 1
 end
