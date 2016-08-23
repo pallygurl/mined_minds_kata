780a10750c50570a5087e8be77c1732bcdec6b03
Cyndi Bolton <cbolton@wvtpg.com>
2016-08-23 13:47:31 -0400

    3 and 5

--- PrintToScreen.rb
+++ PrintToScreen.rb
@@ -2,15 +2,17 @@
  
  x = 1
 100. times do
-#when a number is exactly divisible by 3, replace with Mined
-	if x % 3 == 0
-		puts "Mined"
+	if x % 15 == 0
+		puts "Mined Minds"
 
-#when a number is exactly divisible by 5, replace with  Minds	
+#when a number is exactly divisible by 5 replace with Minds
+	
+	elsif x % 3 == 0
+		puts "Mined"
 	elsif x % 5 == 0
 		puts "Minds"
 
-#when a number is not exactly divisible by 3 or 5, display number	
+#when a number is not exactly divisible by 3 and 5, display number	
 	else
 	puts x	
 	end
